869732d9876174823b8eba25785fce0dc6a699d4
tiffanyjoa8 <46694937+tiffanyjoa8@users.noreply.github.com>
2019-04-21 10:11:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let data;
+
+function hte(){
+    console.log("fsdgrhj");
+    fetch('')
+    .then(function(resp){
+        return resp.json();
+    }).then(function(resp){
+        data = JSON.stringify(resp);
+
+    })
+}
+function hello1(){
+    console.log("fsdgrhj");
+    
+    var text = "The potato is coming out of his cage and doing just fine :)";
+
+    alert(text);
+}
+function hello2(){
+    console.log("fsdgrhj");
+    
+    var text = "The potato is sleeping. Shhh";
+    alert(text);
+}
+
+function hello3(){
+    console.log("fsdgrhj");
+    
+    var text = "The potato is dreaming about world peace, sunsets, and long walks on the beach. ";
+    alert(text);
+}
+
+function hello4(){
+    console.log("fsdgrhj");
+    
+    var text = "Yes!";
+    alert(text);
+}
+
+
+
+function hhte2(){
+    console.log("fsdgrhj");
+    document.getElementById('heh').innerHTML+=' <img src="https://i.pinimg.com/564x/4d/a1/8c/4da18c43deb0d7f0bde7430e76ea5b96.jpg">';
+
+}
+
+
+
